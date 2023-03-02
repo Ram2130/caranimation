f9f6b7c1d1f5212563954cc09c6f11981a344c55
Ram2130 <126746559+Ram2130@users.noreply.github.com>
2023-03-02 17:46:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var audio = document.createElement("audio");
+ audio.setAttribute('src','sound.mp3');
+ audio.loop = true;
+ //audio.play();
+  let wheel = document.getElementById('wheel1');
+  wheel.addEventListener('click' =()=>
+  { 
+
+  })
+ 
+ 
